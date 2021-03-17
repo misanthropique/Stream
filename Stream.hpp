7a5cc30b43b6b40f178f5774cf46e984a63b7169
misanthropique <80142735+misanthropique@users.noreply.github.com>
2021-03-17 18:52:59 -0500

    Need to evaluate the API on paper.

--- Stream.hpp
+++ Stream.hpp
@@ -4,11 +4,27 @@
  * or part by any means, without express prior written agreement is prohibited.
  */
 #pragma once
- 
-template < typename Type >
+
+#include <vector>
+#include <type_traits>
+
+/*
+ * There are a few branching points to consider.
+ * Eager vs Lazy execution, Immutable vs Mutable,
+ * Ordered vs Unordered, and number of threads.
+ * By default the contents of stream shall be mutable and ordered.
+ */
+template < typename Type,
+	bool _Mutable = true,
+	bool _Ordered = true >
 class Stream
 {
+	std::vector< Type > mDataSource;
+
 public:
+	
+	using StreamReturnType = std::conditional< _Mutable, Stream&, Stream >::type;
+
 	Stream()
 	{
 	}
@@ -21,34 +37,106 @@ public:
 	{
 	}
 
+	Stream( std::initializer_list< Type > initList )
+	{
+	}
+
+	Stream( std::vector< Type >&& source )
+	{
+	}
+
+	template < typename Collector >
+	void collect( Collector< Type >& sink )
+	{
+	}
+
 	size_t count( std::function< bool ( const Type& ) > predicate )
 	{
+		// Counting is unaffected by mutability or order
+		// So this is trivially parallelizable.
+		size_t counter( 0 );
+		for ( auto& value : mDataSource )
+		{
+			if ( predicate( value ) )
+			{
+				++counter;
+			}
+		}
+
+		return counter;
 	}
 
-	Stream& filter( std::function< bool ( const Type& ) > predicate )
+	StreamReturnType filter( std::function< bool ( const Type& ) > predicate )
 	{
+		for ( auto iter = mDataSource.begin(); iter != mDataSource.end(); )
+		{
+			if ( predicate( *iter ) )
+			{
+				++iter;
+			}
+			else
+			{
+				iter = mDataSource.erase( iter );
+			}
+		}
+
+		return *this;
 	}
 
-	Stream& foreach( std::function< void ( const Type& ) > function )
+	void foreach( std::function< void ( Type& ) > function )
 	{
+		
 	}
 
-	Stream& foreach( std::function<> function )
+	void foreach( std::function< void ( const Type& ) > function )
 	{
+		for ( auto& value : mDataSource )
+		{
+			function( value );
+		}
+
+		return *this;
 	}
 
 	template < typename MappedType >
 	Stream< MappedType > map( std::function< bool ( MappedType&, const Type& ) > mapper )
 	{
+		Stream< MappedType > mappedStream;
+		MappedType mappedValue;
+
+		for ( auto& value : mDataSource )
+		{
+			if ( mapper( mappedValue, value ) )
+			{
+				mappedStream.mDataSource.push_back( std::move( mappedValue ) );
+			}
+		}
+
+		return std::move( mappedStream );
 	}
 
 	template < typename KeyType >
 	std::map< KeyType, Stream< Type > > partition( std::function< void ( KeyType&, const Type& ) > partitioner )
 	{
+		std::map< KeyType, Stream< Type > > partitionedStreams;
+		KeyType partitionKey;
+
+		for ( auto& value : mDataSource )
+		{
+			partitioner( partitionKey, value );
+			if ( partitionedStreams.end() == partitionedStreams.find( partitionKey ) )
+			{
+			}
+			else
+			{
+				partitionedStreams[ partitionKey ].mDataSource.push_back( std::
+			}
+		}
 	}
 
 	template < typename ReductionType >
-	ReductionType reduce( std::function< void ( ReductionType&, const Type& ) > reducer, ReductionType initializer =  )
+	ReductionType reduce( std::function< void ( ReductionType&, const Type& ) > reducer, ReductionType initializer = ReductionType() )
 	{
+		return std::move( initializer );
 	}
 };
